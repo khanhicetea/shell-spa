57aed596a29334992844293a1bbad8b4d5863979
KhanhIceTea <khanhicetea@gmail.com>
2025-12-13 17:33:33 +0700

    fix getDB

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -5,17 +5,17 @@ import { Pool } from "pg";
 import * as schema from "@/lib/db/schema";
 import { registerQueryTimeLogging } from "./debug";
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  max: parseInt(process.env.DATABASE_MAX_CONNECTIONS || "2", 10),
+const getDatabase = createServerOnlyFn(() => {
+  const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    max: parseInt(process.env.DATABASE_MAX_CONNECTIONS || "2", 10),
+  });
+
+  if (process.env.SQL_LOGGING !== undefined) {
+    registerQueryTimeLogging(pool);
+  }
+
+  return drizzle({ client: pool, schema, casing: "snake_case" });
 });
-
-if (process.env.SQL_LOGGING !== undefined) {
-  registerQueryTimeLogging(pool);
-}
-
-const getDatabase = createServerOnlyFn(() =>
-  drizzle({ client: pool, schema, casing: "snake_case" }),
-);
 
 export const db = getDatabase();
